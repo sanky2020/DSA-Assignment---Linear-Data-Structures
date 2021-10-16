4ed28256c92ebae5e095b68679f49a2537cc1321
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2021-10-16 15:22:06 +0530

    Create Q10.js

---
+++ Q10.js
@@ -0,0 +1,7 @@
+//Write a program to find the smallest number using a stack.
+
+const numbers = [2, 4, 9, 2, 0, 16, 24];
+const smallest_number = Math.min(...numbers);
+const largest_number = Math.max(...numbers);
+console.log('Smallest Value:', smallest_number);
+console.log('Largest Value:', largest_number);
